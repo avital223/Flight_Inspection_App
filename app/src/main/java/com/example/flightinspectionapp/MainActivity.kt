5437efa1f265424073a7d00dc122277c3aa9592f
avital223 <64374818+avital223@users.noreply.github.com>
2021-06-21 13:03:46 +0300

    connect to server

--- app/src/main/java/com/example/flightinspectionapp/MainActivity.kt
+++ app/src/main/java/com/example/flightinspectionapp/MainActivity.kt
@@ -3,19 +3,28 @@ package com.example.flightinspectionapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 
+import com.example.flightinspectionapp.databinding.ActivityMainBinding
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var databinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        databinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         setContentView(R.layout.activity_main)
-//        val mainViewModel = ViewModelProvider(this).get(ViewModelFlight::class.java)
-//        DataBindingUtil.setContentView<ActivityMainBinding>(
-//            this, R.layout.activity_main
-//        ).apply {
-//            this.setLifecycleOwner(this@MainActivity)
-//            this.viewmodel = mainViewModel
-//        }
+//
+        val mainViewModel = ViewModelProvider(this).get(ViewModelFlight::class.java)
+//        databinding.viewmodel = mainViewModel
+//        databinding.lifecycleOwner = this
+
+        DataBindingUtil.setContentView<ActivityMainBinding>(
+            this, R.layout.activity_main
+        ).apply {
+            this.lifecycleOwner = this@MainActivity
+            this.viewmodel = mainViewModel
+        }
 
     }
 }
--- app/src/main/java/com/example/flightinspectionapp/ViewModelFlight.kt
+++ app/src/main/java/com/example/flightinspectionapp/ViewModelFlight.kt
@@ -1,10 +1,13 @@
 package com.example.flightinspectionapp
 
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 
-class ViewModelFlight : ViewModel() {
-    private val modelFlight : ModelFlight = ModelFlight()
+class ViewModelFlight : ViewModel(), Observable {
+    private val modelFlight: ModelFlight = ModelFlight()
     fun onChangeData() {
         for (i in 100 downTo -100 step 1) {
             modelFlight.setAileron((i / 100).toFloat())
@@ -12,5 +15,22 @@ class ViewModelFlight : ViewModel() {
             modelFlight.setRudder((i / 100).toFloat())
 //            modelFlight.setThrottle((i / 100).toFloat())
         }
+    }
+
+    @Bindable
+    val port = MutableLiveData<String>()
+
+    //    @Bindable
+    val _ipServer = MutableLiveData<String>()
+
+    fun connect() {
+        print("Ip: $_ipServer Port: $port")
+        modelFlight.connectToFlight(_ipServer.value.toString(), port.value.toString().toInt())
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
     }
 }
